3a210f9350176fa6d5cf5e48c507f4d0348892c4
fisjac <jfisher9882@gmail.com>
2022-10-13 12:07:59 -0500

    Pokemon model -untested

--- flask-backend/app/models.py
+++ flask-backend/app/models.py
@@ -1,3 +1,22 @@
 from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime as dt
+
 
 db = SQLAlchemy()
+
+
+class Pokemon(db.Model) {
+  id = db.Column(db.Integer, nullable=False)
+  number = db.Column(db.Integer, nullable=False)
+  attack = db.Column(db.Integer, nullable=False)
+  defense = db.Column(db.Integer, nullable=False)
+  image_url = db.Column(db.VARCHAR, nullable=False)
+  name = db.Column(db.VARCHAR, nullable=False, unique=True)
+  type = db.Column(db.VARCHAR, nullable=False)
+  moves= type = db.Column(db.VARCHAR, nullable=False)
+  encounter_rate = db.Column(db.Float(precision=3, decimal=True), default=1)
+  catch_rate = db.Column(db.Float(precision=3, decimal=True), default=1)
+  captured = db.Column(db.Boolean, default=False)
+  updated_at = db.Column(db.DateTime, default=dt.now())
+  updated_at = db.Column(db.DateTime, default=dt.now())
+}
