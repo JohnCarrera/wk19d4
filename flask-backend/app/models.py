a65fde5529101664899a723df9951e2d37239f5a
fisjac <jfisher9882@gmail.com>
2022-10-13 12:30:53 -0500

    db tables created

--- flask-backend/app/models.py
+++ flask-backend/app/models.py
@@ -6,6 +6,8 @@ db = SQLAlchemy()
 
 
 class Pokemon(db.Model):
+  __tablename__ = 'pokemon'
+
   id = db.Column(db.Integer,  primary_key=True)
   number = db.Column(db.Integer, nullable=False)
   attack = db.Column(db.Integer, nullable=False)
@@ -17,5 +19,19 @@ class Pokemon(db.Model):
   encounter_rate = db.Column(db.Float(precision=3), default=1)
   catch_rate = db.Column(db.Float(precision=3), default=1)
   captured = db.Column(db.Boolean, default=False)
+  created_at = db.Column(db.DateTime, default=dt.now())
   updated_at = db.Column(db.DateTime, default=dt.now())
+
+  items = db.relationship('items.id', back_populates='pokemon')
+
+class Items(db.Model):
+  __tablename__ = 'items'
+
+  id = db.Column(db.Integer, primary_key=True)
+  happiness = db.Column(db.Integer, nullable=False)
+  image_url = db.Column(db.VARCHAR, nullable=False)
+  name = db.Column(db.VARCHAR, nullable=False)
+  price = db.Column(db.Integer, nullable=False)
+  pokemon_id = db.Column(db.Integer, db.ForeignKey('pokemon.id'))
+  created_at = db.Column(db.DateTime, default=dt.now())
   updated_at = db.Column(db.DateTime, default=dt.now())
