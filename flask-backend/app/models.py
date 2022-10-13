470fd2d8f26d7a5fc18a44511eb93668f76d7ff2
fisjac <jfisher9882@gmail.com>
2022-10-13 12:16:05 -0500

    initialized migrations folder

--- flask-backend/app/models.py
+++ flask-backend/app/models.py
@@ -5,8 +5,8 @@ from datetime import datetime as dt
 db = SQLAlchemy()
 
 
-class Pokemon(db.Model) {
-  id = db.Column(db.Integer, nullable=False)
+class Pokemon(db.Model):
+  id = db.Column(db.Integer,  primary_key=True)
   number = db.Column(db.Integer, nullable=False)
   attack = db.Column(db.Integer, nullable=False)
   defense = db.Column(db.Integer, nullable=False)
@@ -14,9 +14,8 @@ class Pokemon(db.Model) {
   name = db.Column(db.VARCHAR, nullable=False, unique=True)
   type = db.Column(db.VARCHAR, nullable=False)
   moves= type = db.Column(db.VARCHAR, nullable=False)
-  encounter_rate = db.Column(db.Float(precision=3, decimal=True), default=1)
-  catch_rate = db.Column(db.Float(precision=3, decimal=True), default=1)
+  encounter_rate = db.Column(db.Float(precision=3), default=1)
+  catch_rate = db.Column(db.Float(precision=3), default=1)
   captured = db.Column(db.Boolean, default=False)
   updated_at = db.Column(db.DateTime, default=dt.now())
   updated_at = db.Column(db.DateTime, default=dt.now())
-}
